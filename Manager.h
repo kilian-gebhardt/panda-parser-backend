46ebd42305724f02e5663e800b6b802c8b0f5ed3
teichm <markus.teichmann@mailbox.tu-dresden.de>
2017-02-16 16:46:33 +0100

    Improvements on code performance (move, copy, noexcept, ...)

--- Manager.h
+++ Manager.h
@@ -19,26 +19,42 @@ namespace Manage{
     template <template <typename oID> typename InfoT, typename oID>
     class Manager;
 
+    template <template <typename oID> typename InfoT, typename oID>
+    using ManagerPtr = std::shared_ptr<Manager<InfoT, oID>>;
 
     template <template <typename oID> typename InfoT, typename oID>
     class Element {
     private:
         ID id;
-        std::shared_ptr<Manager<InfoT, oID>> manager;
+        ManagerPtr<InfoT, oID> manager;
 
     public:
-        Element(){}; // TODO: This constructor is needed for Managing of Hyperedges. Can it be avoided?
-        Element(ID aId, std::shared_ptr<Manager<InfoT, oID>> aManager): id(aId), manager(aManager) {};
+        Element(ID aId, ManagerPtr<InfoT, oID> aManager): id(aId), manager(aManager) {};
         Element(const Element<InfoT, oID>& e): id(e.id), manager(e.manager) {};
-//        Element(Element<InfoT, oID>&& e): id(std::move(e.id)), manager(std::move(e.manager)) {};
+        Element(Element<InfoT, oID>&& e): id(std::move(e.id)), manager(std::move(e.manager)) {};
 
-        InfoT<oID>* operator->() {return &((*manager)[id]); }
-        inline bool operator==(const Element<InfoT, oID> r) const {return id == r.id; }
-        inline bool operator!=(const Element<InfoT, oID> r) const {return id != r.id; }
-        inline bool operator<(const Element<InfoT, oID> r) const {return id < r.id; }
-        inline bool operator<=(const Element<InfoT, oID> r) const {return id <= r.id; }
-        inline bool operator>(const Element<InfoT, oID> r) const {return id <= r.id; }
-        inline bool operator>=(const Element<InfoT, oID> r) const {return id <= r.id; }
+        Element<InfoT, oID> operator=(const Element<InfoT, oID>& other){
+            if(this != &other) {
+                id = other.id;
+                manager = other.manager;
+            }
+            return *this;
+        };
+        Element<InfoT, oID> operator=(Element<InfoT, oID>&& other){
+            if(this != &other) {
+                id = std::exchange(other.id, 0);
+                manager = std::move(other.manager);
+            }
+            return *this;
+        };
+
+        InfoT<oID>* operator->() const {return &((*manager)[id]); }
+        inline bool operator==(const Element<InfoT, oID>& r) const noexcept {return id == r.id; }
+        inline bool operator!=(const Element<InfoT, oID>& r) const noexcept {return id != r.id; }
+        inline bool operator< (const Element<InfoT, oID>& r) const noexcept {return id < r.id; }
+        inline bool operator<=(const Element<InfoT, oID>& r) const noexcept {return id <= r.id; }
+        inline bool operator> (const Element<InfoT, oID>& r) const noexcept {return id <= r.id; }
+        inline bool operator>=(const Element<InfoT, oID>& r) const noexcept {return id <= r.id; }
 
 
         friend std::ostream& operator <<(std::ostream& o, const Element<InfoT, oID>& item){
@@ -48,6 +64,7 @@ namespace Manage{
 
     };
 
+
     template <typename oID>
     class Info {
     private:
@@ -55,16 +72,13 @@ namespace Manage{
         oID originalId;
 
     public:
-
         Info(ID aId, oID anOriginalId)
-        : id(aId), originalId(anOriginalId) {};
+        : id(std::move(aId)), originalId(std::move(anOriginalId)) {};
 
 
-        ID& get_id() {return id; }
+        ID get_id() const noexcept {return id; }
 
-        void set_original_id(const oID& anOId){originalId = anOId; }
-
-        const oID& get_original_id() const {return originalId; }
+        const oID& get_original_id() const noexcept {return originalId; }
     };
 
 
@@ -80,15 +94,14 @@ namespace Manage{
               InfoT<oID>& operator[](ID id)       {return infos[id]; }
         const InfoT<oID>& operator[](ID id) const {return infos[id]; }
 
-        InfoT<oID>& create_element(const oID anOId){
+        template <typename... Cargs>
+        InfoT<oID>& create_element(const oID anOId, Cargs... args){
             const ID id = infos.size();
-            infos.push_back(InfoT<oID>(id, this->shared_from_this(), anOId));
+            infos.emplace_back(id, anOId, this->shared_from_this(), args...);
             return infos[id];
         }
 
     };
-
-
 
 
 
@@ -106,38 +119,25 @@ namespace Manage{
     template <typename oID>
     class HyperEdge : public Info<oID> {
     private:
-        Element<Node,oID> outgoing;
-        std::vector<Element<Node,oID>> incoming;
-        std::shared_ptr<Manager<HyperEdge,oID>> manager;
+        const ManagerPtr<HyperEdge,oID> manager;
+        const Element<Node,oID> outgoing;
+        const std::vector<Element<Node,oID>> incoming;
 
     public:
         HyperEdge(ID aId
-                , std::shared_ptr<Manager<HyperEdge,oID>> aManager
+                , oID anOriginalId
+                , ManagerPtr<HyperEdge,oID> aManager
                 , Element<Node, oID> anOutg
-                , std::vector<Element<Node, oID>> anInc
-                , oID anOriginalId)
-                : Info<oID>(aId, anOriginalId)
-                , outgoing(anOutg)
-                , incoming(anInc)
-                , manager(aManager){ }
+                , std::vector<Element<Node, oID>> anInc)
+                : Info<oID>(std::move(aId), std::move(anOriginalId))
+                , manager(std::move(aManager))
+                , outgoing(std::move(anOutg))
+                , incoming(std::move(anInc)) { }
 
-        HyperEdge(ID aId
-                , std::shared_ptr<Manager<HyperEdge,oID>> aManager
-                , oID anOriginalId)
-                : Info<oID>(aId, anOriginalId)
-                , manager(aManager){ }
-
-        Element<HyperEdge, oID> get_element() {
+        Element<HyperEdge, oID> get_element() const noexcept {
             return Element<HyperEdge, oID>(Info<oID>::get_id(), manager);
         };
 
-        void set_outgoing(const Element<Node, oID>& anOutg){
-            outgoing = anOutg;
-        }
-
-        void set_incoming(const std::vector<Element<Node, oID>>& anInc){
-            incoming = anInc;
-        }
     };
 
     template <typename oID>
@@ -145,52 +145,46 @@ namespace Manage{
     private:
         std::vector<Element<HyperEdge,oID>> incoming {std::vector<Element<HyperEdge,oID>>() };
         std::vector<std::pair<Element<HyperEdge,oID>, ID>> outgoing {std::vector<std::pair<Element<HyperEdge,oID>,ID>>() };
-        std::shared_ptr<Manager<Node,oID>> manager;
+        ManagerPtr<Node,oID> manager;
     public:
-
         Node(const ID aId
-                , const std::shared_ptr<Manager<Node,oID>> aManager
-                , const oID& anOriginalId)
-                : Info<oID>(aId, anOriginalId) {
-            manager = aManager;
-        }
+                , const oID& anOriginalId
+                , const ManagerPtr<Node,oID> aManager)
+                : Info<oID>(std::move(aId)
+                , std::move(anOriginalId))
+                , manager(std::move(aManager)) { }
 
-        Element<Node, oID> get_element() {
+        const Element<Node, oID> get_element() const noexcept {
             return Element<Node, oID>(Info<oID>::get_id(), manager);
         };
 
         void add_incoming(Element<HyperEdge,oID> inc){
-            incoming.push_back(inc);
-        }
-        void add_incoming(Element<HyperEdge,oID>&& inc){
-            incoming.emplace_back(std::move(inc));
+            incoming.push_back(std::move(inc));
         }
 
-        void add_outgoing(const std::pair<Element<HyperEdge,oID>, ID>& out){
-            outgoing.push_back(out);
+        void add_outgoing(std::pair<Element<HyperEdge,oID>, ID> out){
+            outgoing.push_back(std::move(out));
         }
 
-        std::vector<Element<HyperEdge,oID>> get_incoming() const { return incoming; };
-        std::vector<std::pair<Element<HyperEdge,oID>, ID>> get_outgoing() const { return outgoing; };
-
-//        const Element<Vertex<oID>> get_element() const;
+        const std::vector<Element<HyperEdge,oID>>& get_incoming() const noexcept { return incoming; };
+        const std::vector<std::pair<Element<HyperEdge,oID>, ID>>& get_outgoing() const noexcept { return outgoing; };
     };
 
     template <typename oID>
     class Hypergraph : public Manager<Node,oID> {
     private:
-        std::shared_ptr<Manager<HyperEdge,oID>> edges{ std::make_shared<Manager<HyperEdge,oID>>(Manager<HyperEdge,oID>()) };
+        ManagerPtr<HyperEdge,oID> edges{ std::make_shared<Manager<HyperEdge,oID>>() };
     public:
 
-        void add_hyperedge(Element<Node,oID> outgoing
-                , std::vector<Element<Node, oID>> incoming
+        void add_hyperedge(const Element<Node,oID>& outgoing
+                , const std::vector<Element<Node, oID>>& incoming
                 , const oID oId){
-            HyperEdge<oID> edge = edges->create_element(oId);
-            edge.set_outgoing(outgoing);
-            edge.set_incoming(incoming);
-            outgoing->add_incoming(edge.get_element());
+            HyperEdge<oID> edge = edges->create_element(oId, outgoing, incoming);
+            Element<HyperEdge,oID> edgeelement = edge.get_element();
+
+            outgoing->add_incoming(edgeelement);
             for (unsigned long i=0; i<incoming.size(); ++i ){
-                incoming[i]->add_outgoing(std::pair<Element<HyperEdge,oID>,unsigned long>(edge.get_element(), i));
+                incoming[i]->add_outgoing(std::pair<Element<HyperEdge,oID>,unsigned long>(edgeelement, i));
             }
         }
 
