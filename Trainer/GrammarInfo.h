68e91d3ca665c66c9b7e8b09b2e9a61625f48fce
Kilian Gebhardt <kilian.gebhardt@tu-dresden.de>
2017-03-13 16:07:29 +0100

    GrammarInfo: made normalization groups const

--- Trainer/GrammarInfo.h
+++ Trainer/GrammarInfo.h
@@ -12,14 +12,21 @@ namespace Trainer {
     class GrammarInfo2 {
     public:
         std::vector<std::vector<size_t>> rule_to_nonterminals;
-        std::vector<std::vector<size_t>> normalizationGroups;
+        const std::vector<std::vector<size_t>> normalizationGroups;
         const size_t start;
 
         GrammarInfo2(
                 const std::vector<std::vector<size_t>> rule_to_nonterminals
                 , size_t start
         )
-                : rule_to_nonterminals(rule_to_nonterminals), start(start) {
+                : rule_to_nonterminals(rule_to_nonterminals)
+                , normalizationGroups(GrammarInfo2::compute_normalization_groups(rule_to_nonterminals))
+                , start(start) {
+        }
+
+    private:
+        static std::vector<std::vector<size_t>> compute_normalization_groups(
+                const std::vector<std::vector<size_t>> & rule_to_nonterminals) {
             std::vector<std::vector<size_t>> normGroups;
             for (size_t rule_idx = 0; rule_idx < rule_to_nonterminals.size(); ++rule_idx) {
                 if (rule_to_nonterminals[rule_idx].size() > 0) {
@@ -29,8 +36,7 @@ namespace Trainer {
                     normGroups[rule_to_nonterminals[rule_idx][0]].push_back(rule_idx);
                 }
             }
-
-            normalizationGroups = normGroups;
+            return normGroups;
         }
     };
 
